3b6901737319ecc79c907c92671bff7395a9b34b
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-02-28 16:28:18 +0700

    Update_LoanRespository_1

--- src/repository/LoanRepository.ts
+++ src/repository/LoanRepository.ts
@@ -1,7 +1,5 @@
-
-import { PrismaClient, Book, Loan } from '@prisma/client';
-
-
+import { PrismaClient, Loan } from '@prisma/client';  
+import { Loan as LoanModel } from '../models/Loan'; 
 export class LoanRepository {
   private prisma: PrismaClient;
 
@@ -9,31 +7,38 @@ export class LoanRepository {
     this.prisma = new PrismaClient();
   }
 
+  // ยืมใหม่
   async createLoan(loanData: { bookId: number; memberId: number; quantity: number; dueDate: Date }): Promise<Loan> {
-    return await this.prisma.loan.create({
+    return this.prisma.loan.create({
       data: {
-        bookId: loanData.bookId,
-        memberId: loanData.memberId,
-        quantity: loanData.quantity,
-        dueDate: loanData.dueDate,
-
+        ...loanData,  
       },
     });
   }
 
+  // ดึงข้อมูลการยืม
   async getLoansByDueDate(dueDate: Date): Promise<Loan[]> {
-    return await this.prisma.loan.findMany({
+    return this.prisma.loan.findMany({
       where: {
         dueDate: dueDate,
-        returnDate: null,
+        returnDate: null,  
+      },
+      include: {
+        book: true, 
+        member: true,  
       },
     });
   }
 
+  // ดึงข้อมูลการยืมที่ยังไม่ได้คืน
   async getLoansNotReturned(): Promise<Loan[]> {
-    return await this.prisma.loan.findMany({
+    return this.prisma.loan.findMany({
       where: {
-        returnDate: null,
+        returnDate: null,  
+      },
+      include: {
+        book: true,  
+        member: true, 
       },
     });
   }
