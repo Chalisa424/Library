934c0bc40555d5020665be356dcdc251e4260c75
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-03-10 02:32:05 +0700

    Lab05_Task1.6

--- src/routes/BookRoutes.ts
+++ src/routes/BookRoutes.ts
@@ -4,6 +4,19 @@ import { BookAPI } from '../api/BookAPI';
 const router = express.Router();
 const bookAPI = new BookAPI();
 
+// ดึงข้อมูลหนังสือทั้งหมด (พร้อมรองรับ pagination)
+router.get('/', async (req, res) => {
+    if (req.query.pageSize && req.query.pageNo) {
+      // ถ้ามีการส่ง pageSize และ pageNo มา ให้ทำ pagination
+      const pageSize = parseInt(req.query.pageSize as string);
+      const pageNo = parseInt(req.query.pageNo as string);
+      res.json(await bookAPI.getAllBooksWithPagination(pageSize, pageNo));
+    } else if (req.query.category){
+      // ถ้าไม่มี pageSize และ pageNo ให้ดึงข้อมูลหนังสือทั้งหมด
+     const category = req.query.category;
+    }
+  });
+
 router.get('/', bookAPI.getAllBooks.bind(bookAPI)); // ดึงข้อมูลหนังสือทั้งหมด
 router.get('/search', bookAPI.searchBooks.bind(bookAPI)); // ค้นหาหนังสือตามชื่อ
 router.get('/due-date', bookAPI.getBooksByDueDate.bind(bookAPI)); // ค้นหาหนังสือตามกำหนดการคืน
